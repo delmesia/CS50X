ef1765a9864b7906a59a0f85a70fe289b90763c3
Del <wendellsmesia@gmail.com>
2023-08-26 15:38:31 +0800

    Added some comments

--- Substition.c
+++ Substition.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define ALPHABET 26
 int main(){
   const char letters[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q'};
-  char *cipher = malloc(26);
-  char *s = malloc(5);
-  char *cip = malloc(5);
+  char *cipher = malloc(26); 	//This will allocation enough space for the cipher key
+  char *s = malloc(26);		//Plaint text that the user will provide
+  char *cip = malloc(26);	//The cipher key that will be printed to stdout.
   puts("Enter the cipher code: ");
   scanf("%s", cipher);
   puts("Enter the Plain Text: ");
   scanf("%s", s);
   for(size_t i = 0; i < sizeof(letters)*sizeof(char);i++){
     for(size_t j = 0; j < sizeof(letters)*sizeof(char); j++) {
-     if(s[i] == letters[j]) {
-      cip[i] += cipher[j];
+     if(s[i] == letters[j]) { 	
+      cip[i] += cipher[j];	// For each character in *s, find the equivalent in letters[] array
+				// and keep the index to map it to the cipher key that will be printed
       }
     }
   }
